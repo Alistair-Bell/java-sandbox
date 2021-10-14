3ffce65fe179573b671284ab1be43d90a0ebfd86
Alistair <alistair@alistairbell.xyz>
2021-10-14 15:12:30 +0100

    Update to blackjack game!

--- blackjack.java
+++ blackjack.java
@@ -1,58 +1,91 @@
 import java.util.*;
 
-/* Have the card be a regular integer, this allows the use of built in algorithms. */
 final class card {
-	public static final int suit_mask  = 0xf;
-	public static final int value_mask = 0xf0;
-	public static final int alt_mask   = 0xf00;
-	public static String[] suit_names  = { "diamond", "heart", "club", "spade" };
-
-	public static int construct(int __suit, int __value) {
-		/* Set the suit using the mask, not shifts currently required. */
-		int return_value = __suit & suit_mask;
-
-		/* All royality cards are set to 10. */
-		if (9 < __value) {
+	public byte _value;
+	public byte _suit;
+	public byte _alt;
+	public static String[] suit_names = { "diamonds", "hearts", "clubs", "spades" };
+	
+	public card(int __value, int __suit, int __alt) {
+		if (__value > 9) {
 			__value = 10;
-		} else if (__value == 1) {
-			/* Special rule for the ace card. */
-			return_value += (11 << 8);
 		}
-		return_value += (__value << 4);	
-		return return_value;
+		_value = ((byte)__value);
+		_suit  = ((byte)__suit);
+		_alt   = ((byte)__alt);
+	}
+	public static int win(String __player, card __c1, card __c2) {
+		if (__c1._value + __c2._value == 21) {
+			System.out.printf("%s won, they had the winning hand to sum to 21!\n", __player);
+			return 1;
+		} else if ((__c1._alt + __c2._value) == 21 || (__c1._value + __c2._alt) == 21) {
+			System.out.printf("%s won, their cards alternate values won them the game!\n", __player);
+			return 1;
+		}
+		return 0;
+	}
+	public static boolean win(card __c1, card __c2, card __c3) {
+		return false;
 	}
 }
 final class deck {
-	public int[] _cards;
-	public int _drawn;
-	
-	public deck() {
-		/* Construct the deck. */
-		_cards = new int[52];
+	public card[] _cards;
+	public int    _drawn;
+
+	public void reconstruct() {
+		if (_cards == null) {
+			_cards = new card[52];
+		}
 		_drawn = 0;
 
-		int i = 0, j;	
-		for (; i < 4; ++i) {
+		int i, j;
+		for (i = 0; i < 4; ++i) {
 			for (j = 0; j < 13; ++j) {
-				_cards[j + (i * 13)] = card.construct(i, j + 1);
+				_cards[j + (i * 13)] = new card(j + 1, i, (j == 0) ? 11 : 0);
 			}
 		}
 	}
-	public int draw() {
-		return 0;
+	public card draw() {
+		if (_drawn == _cards.length) {
+			return new card(0, 0, 0); 
+		}
+		++_drawn;
+		return _cards[_drawn - 1];
 	}
 	public void shuffle() {
+		/* This random generator is not that random, really could use some lava lamps. */
+		Random random = new Random();
+		for (int i = 0; i < 10; ++i) {
+			for (int j = 0; j < _cards.length; ++j) {
+				int swap_index = random.nextInt(_cards.length);
+				card current_value = _cards[i];
+				_cards[i]          = _cards[swap_index];
+				_cards[swap_index] = current_value;
+			}
+		}
+	}
+	public deck() {
+		reconstruct();
 	}
 }
 
 class blackjack_program {
-	public static Scanner _scanner = new Scanner(System.in);
 	public static void main(String[] __argv) {
-		deck playing_cards = new deck();
-		int i = 0;
-		for (; i < playing_cards._cards.length; ++i) {
-			int s = playing_cards._cards[i];
-			System.out.printf("Card %2d, suit: %s, value: %2d, alt: %2d\n", i + 1, card.suit_names[s & card.suit_mask], (s & card.value_mask) >> 4, (s & card.alt_mask) >> 8);
+		deck d = new deck();
+		d.shuffle();
+
+		card[] pcards = { d.draw(), d.draw(), new card(0, 0, 0) };
+		card[] dcards = { d.draw(), d.draw(), new card(0, 0, 0) };
+
+		System.out.printf("You drew a %d of %s, %d of %s, total %d.\n",
+			pcards[0]._value, card.suit_names[pcards[0]._suit],
+			pcards[1]._value, card.suit_names[pcards[1]._suit], pcards[0]._value + pcards[1]._value);
+		System.out.printf("Dealer drew a %d of %s, %d of %s, total %d.\n",
+			dcards[0]._value, card.suit_names[dcards[0]._suit],
+			dcards[1]._value, card.suit_names[dcards[1]._suit], dcards[0]._value + dcards[1]._value);
+
+		if (card.win("You", pcards[0], pcards[1]) == 1 || card.win("Dealer", dcards[0], dcards[1]) == 1) {
+			return;
 		}
 	}
 }
