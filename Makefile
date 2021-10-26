f9856eb1d222d8c19f1069c5ad688998ad81399f
Alistair <alistair@alistairbell.xyz>
2021-10-26 20:41:49 +0000

    Added check digit program!
    Uses some wacky algorithm given to use and then impliment within java.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 JC = javac
 JA = java
 
-TARGETS = employee shapes compression guessing sequence_reverse dice_roll blackjack treasure_game
+TARGETS = employee shapes compression guessing sequence_reverse dice_roll blackjack treasure_game check_digit
 
 all: info
 info:
@@ -24,6 +24,9 @@ blackjack:
 	${JC} blackjack.java && ${JA} blackjack_program
 treasure_game:
 	${JC} treasure_game.java && ${JA} treasure_game_program
+check_digit:
+	${JC} check_digit.java && ${JA} check_digit_program
+
 
 
 
---
+++ check_digit.java
@@ -0,0 +1,20 @@
+class check_digit_program { 
+
+	public static int generate_digit(final byte[] __bytes) {
+		int even_sum = 0, odd_sum = 0;
+		for (int i = 0; i < __bytes.length; ++i) {
+			if ((i & 1) == 0) {
+				even_sum += __bytes[i];
+			} else {
+				odd_sum += __bytes[i];
+			}
+		}
+		int pre = (odd_sum * 3) + even_sum;
+		return (pre + (10 - pre % 10)) - pre;
+	}
+
+	public static void main(String[] __argv) {
+		final byte[] bytes = { 9, 3, 1, 1, 9, 6, 0, 0, 4, 3, 7, 1 };
+		System.out.printf("Check digit [%d]\n", generate_digit(bytes));
+	} 
+}
