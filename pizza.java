9379ebfb8baa713bcdc90b2040c1d5e01f829cf9
Alistair <alistair@alistairbell.xyz>
2021-11-07 16:09:21 +0000

    Update to the pizza program!
    Signed-off-by: Alistair <alistair@alistairbell.xyz>

--- pizza.java
+++ pizza.java
@@ -10,6 +10,8 @@ class pizza_program {
 		"(s)mall",
 		"(m)edium",
 		"(l)arge",
+		"(j)umbo",
+		"(c)omical",
 	};
 	public static String[] _toppings = {
 		"(a)nchovies",
@@ -19,6 +21,10 @@ class pizza_program {
 		"(o)lives",
 		"(p)epper",
 		"(g)arlic",
+		"(h)am",
+		"(P)ineapple",
+		"(P)epperoni",
+		"(A)lmonds",
 		"(n)one",
 	};
 	public static Scanner _in = new Scanner(System.in);
@@ -29,7 +35,7 @@ class pizza_program {
 			System.out.println(s);
 		}
 	}
-	public static int get_list(String[] _values) {
+	public static int get_list(String[] __values, String __header) {
 		do {
 			char search = (char)0;
 			try {
@@ -39,12 +45,14 @@ class pizza_program {
 				_in.nextLine();
 			}
 			finally {
-				for (int i = 0; i < _values.length; ++i) {
-					if (_values[i].charAt(1) == search) {
+				for (int i = 0; i < __values.length; ++i) {
+					if (__values[i].charAt(1) == search) {
 						return i;
 					}
 				}
 			}
+			System.out.printf("Invalid input, enter a value from the list!\n");
+			display_list(__header, __values);
 		} while (true);
 	}
 
@@ -53,18 +61,28 @@ class pizza_program {
 		long pennies = 0;
 		int topping_count = 0;
 		display_list("Sizes", _sizes);
-		pennies += 799 + (get_list(_sizes) * 300);
-		do {
+		pennies += 799 + (get_list(_sizes, "Sizes") * 300);
+
+		HashMap<Integer, Boolean> topping_map = new HashMap<Integer, Boolean>();
+
+	 	do {
 			display_list("Toppings", _toppings);
-			if (get_list(_toppings) == _toppings.length - 1) {
+			int index = get_list(_toppings, "Toppings");
+			if (index == _toppings.length - 1) {
 				break;
 			}
-			++topping_count;
+			if (topping_map.get(index) != null) {
+				System.out.printf("Topping already added!, please select another.\n");
+			} else {
+				topping_map.put(index, true);
+				++topping_count;
+			}
 		} while (true);
 
+		long before_price = pennies;
 		if (topping_count > 2) {
 			pennies += ((topping_count - 2) * 75);
 		}
-		System.out.printf("Total %.2f.\n", ((float)pennies / 100));
+		System.out.printf("Total %d great british pennies, an extra %d for %d toppings on pizza.\n", pennies, pennies - before_price, topping_count);
 	}
 }
