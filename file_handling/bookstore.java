cfb84964f25d8baabb48d904a2409316b64857ed
Alistair <alistair@alistairbell.xyz>
2021-12-09 19:55:16 +0000

    Login system to bookstore.
    Creating accounts coming soon!
    Signed-off-by: Alistair <alistair@alistairbell.xyz>

--- file_handling/bookstore.java
+++ file_handling/bookstore.java
@@ -12,6 +12,78 @@
 import java.util.*;
 import java.io.*;
 
+final class account {
+	String _name;
+	String _email;
+	int _hash;
+	int _perms;
+
+	public account() {
+		_name  = "root";
+		_hash  = "pass".hashCode();
+		_email = "root@bookstore.org";
+		_perms = Integer.MAX_VALUE;
+	}
+	public account(String __name, String __password, String __email, int __perms) {
+		_name  = __name;
+		_hash  = __password.hashCode();
+		_email = __email;
+		_perms = __perms;
+	}
+	public account(String __raw) {
+		String[] vals = __raw.split(":");
+		_name  = vals[0];
+		_hash  = Integer.valueOf(vals[1]);
+		_email = vals[2];
+		_perms = Integer.valueOf(vals[3]);
+	}
+	/* Override. */
+	public String toString() {
+		return _name + ":" + _hash + ":" + _email + ":" + _perms;
+	}
+}
+
+final class login {
+	public static final String out = "shadow.txt";
+	
+	private File _file;
+	public login() {
+		_file = new File(out);	
+		try {
+			_file.createNewFile();
+		} catch (Exception e) {
+		}
+	}
+	public List<account> load() {
+		List<account> ret = new ArrayList<account>();
+		try {
+			Scanner s = new Scanner(_file);
+			while (s.hasNextLine()) {
+				ret.add(new account(s.nextLine()));
+			}
+			s.close();
+		} catch (Exception e) {
+			System.out.printf("Exception thrown %s.\n", e.toString());
+			return null;
+		}
+		return ret;
+	}
+	public int dump(List<account> __in) {
+		try	{
+			FileWriter f = new FileWriter(out, false);
+			for (account a : __in)
+				f.write(a.toString());
+			f.write("\n");
+			f.close();
+		} catch (Exception e) {
+			System.out.printf("Exception thrown %s.\n", e.toString());
+			return -1;
+		}
+		return 0;
+	}
+}
+
+
 final class book {
 	enum genre {
 		ADVENTURE, CRIME, ROMANCE, NON_FICTION, BIOGRAPHY, HISTORICAL, ACTION
@@ -33,7 +105,7 @@ final class book {
 	public boolean write() {
 		FileWriter writer;
 		try {
-			writer = new FileWriter(bookstore_program._file_name, true);
+			writer = new FileWriter(bookstore_program._file_name, false);
 			/* Write all the book data. */
 			writer.write(_title + ",");
 			writer.write(_isbn + ",");
@@ -82,35 +154,52 @@ final class book {
 		System.out.printf("Book [%s] [%s] [%s] [%s].\n", _title, _genre.toString(), _isbn, Arrays.toString(_author));
 	}
 }
+final class library {
+	
+}
 
 class bookstore_program {
 	public static String _file_name = "books.txt";
 	public static File _file = new File(_file_name);
-	public static Scanner _scanner = new Scanner(System.in);
+	private static login _sys = new login();
+	private static Scanner _scanner = new Scanner(System.in);
 
-	private static boolean create_file() {
-		try {
-			_file.createNewFile();
-			return true;
-		} catch (Exception e) {
-			System.out.printf("Exception thrown %s.\n", e.toString());
-			return false;
+	
+	public static int account_index(List<account> __in, String __name) {
+		for (int i = 0; i < __in.size(); ++i) {
+			if (__in.get(i)._name.equals(__name)) {
+				return i;
+			}
 		}
+		return -1;
 	}
-	public static void main(String[] __argv) {
-		/* Create the file if it does not already exist. */
-		if (!create_file()) {
-			return;
+	public static account login() {
+		/* Load our logins. */
+		List<account> accounts = _sys.load();
+		if (accounts == null || accounts.size() < 1) {
+			accounts = new ArrayList<account>();
+			accounts.add(new account());
+			_sys.dump(accounts);
 		}
-		/* Predefined books. */
-		final book[] books = {
-			new book("The C Programming Language", "9780131101630", new String[] { "Brian Kernighan", "Dennis Ritchie" }, book.genre.NON_FICTION),
-			new book("The Cathedral and the Bazaar", "1-565-92724-9", new String[] { "Eric S. Raymond" }, book.genre.NON_FICTION),
-		};
-		for (book b : books) {
-			b.write();
-		}	
-
-		List<book> in = book.read();
+		
+		/* Get the name from our user. */
+		do {
+			System.out.println("What is your name?");
+			String name = _scanner.nextLine();
+			int index;
+			if ((index = account_index(accounts, name)) != -1) {
+				System.out.println("What is the password?");
+				if (_scanner.nextLine().hashCode() == accounts.get(index)._hash)
+					return accounts.get(index);
+				System.out.printf("Invalid password for %s.\n", name);
+			} else {
+				System.out.printf("Unable to find account with name %s.\n", name);
+			}
+		} while (true);
+	}
+	
+	public static void main(String[] __argv) {
+		account a = login();
+		System.out.printf("Hello %s, %s.\n", a._name, a._email);
 	}
 }
